73f76f430defe006935b5c17e58401623ae9bd13
Omitul <omitul1010@gmail.com>
2024-10-01 04:06:58 +0600

    added post validation schema

---
+++ src/app/modules/posts/post.validation.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const CreatePostValidationSchema = z.object({
+  body: z.object({
+    title: z.string().min(1),
+    content: z.string().min(1),
+    author: z.string().length(24),
+    category: z.string().min(1),
+    tags: z.array(z.string()),
+    isPremium: z.boolean().default(false).optional(),
+    images: z.array(z.string()).optional(),
+    videos: z.array(z.string()).optional(),
+  }),
+});
+
+export const UpdatePostValidationSchema = z.object({
+  body: z.object({
+    title: z.string().min(1).optional(),
+    content: z.string().min(1).optional(),
+    author: z.string().length(24).optional(),
+    category: z.string().min(1).optional(),
+    tags: z.array(z.string()).optional(),
+    isPremium: z.boolean().optional(),
+    images: z.array(z.string()).optional(),
+    videos: z.array(z.string()).optional(),
+  }),
+});
+
+export const PostValidation = {
+  CreatePostValidationSchema,
+  UpdatePostValidationSchema,
+};
