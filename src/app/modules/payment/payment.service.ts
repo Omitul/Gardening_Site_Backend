3da21ef0cbe5fef2886bac625e56bea0c2521124
Omitul <omitul1010@gmail.com>
2024-10-13 09:17:00 +0600

    done everything, final push

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -6,7 +6,7 @@ const ConfirmationService = async (transactionId: string, status: string) => {
 
   const verify_res = await verifyPayment(transactionId);
 
-  console.log(verify_res);
+  // console.log(verify_res);
 
   let res;
   if (verify_res && verify_res.pay_status === 'Successful') {
